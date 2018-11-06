5d498f98478eb2fd5aa895830741eb2092e61100
Hamza Berrada <33551619+hamzaBerrada@users.noreply.github.com>
2018-11-06 10:53:15 +0100

    correction

--- src/org/mql/platform/dao/ActivityRepository.java
+++ src/org/mql/platform/dao/ActivityRepository.java
@@ -6,12 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /**
  * @author chermehdi
  */
-public interface ActivityRepository extends JpaRepository<Activity, String> {
+public interface ActivityRepository extends JpaRepository<Activity, Long> {
 
 }
-//we can redefine methodes's mother class  
-//findBy id, findBy ..
-//repositoryLOI
-// personDao  ---> personDaoRepository.
-//we don't need this anymore
-//class and id of class
