06242617981237bd7ec0666e0a54e990f5ec8080
tahrisqalli <33979691+tahrisqalli@users.noreply.github.com>
2018-11-06 08:38:13 +0000

    bba

--- src/org/mql/platform/dao/ActivityRepository.java
+++ src/org/mql/platform/dao/ActivityRepository.java
@@ -9,3 +9,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ActivityRepository extends JpaRepository<Activity, Integer> {
 
 }
+// on peut redefinier les methodes de la classe mere
+//findBy id, findBy ..
+//repository. mais il n'existe
+
+// personDao  --- personDaoRepository.
+//mnt plus besoind de sa 
+//intefger le type de l'identifiena de la classe'
