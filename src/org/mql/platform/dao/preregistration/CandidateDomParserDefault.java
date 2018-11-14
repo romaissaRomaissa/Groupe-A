6ce6bba532504f765f0b0ec9681fcf70a6b35e2e
chermehdi <mehdi.cheracher@gmail.com>
2018-11-14 09:42:10 +0100

    wip

--- src/org/mql/platform/dao/preregistration/CandidateDomParserDefault.java
+++ src/org/mql/platform/dao/preregistration/CandidateDomParserDefault.java
@@ -2,7 +2,6 @@ package org.mql.platform.dao.preregistration;
 
 import java.io.File;
 import java.io.PrintWriter;
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Vector;
 import org.mql.platform.models.preregistration.*;
@@ -69,7 +68,7 @@ public class CandidateDomParserDefault implements CandidateDomParser {
       candidate.setCne(a.getChild("cne").getIntValue());
       candidate.setLastName(a.getChild("lastName").getValue());
       candidate.setFirstName(a.getChild("firstName").getValue());
-      candidate.setAge(a.getChild("age").getIntValue());
+      candidate.setYearOfBirth(a.getChild("age").getIntValue());
       candidate.setSituation(a.getChild("situation").getValue());
       candidate.setCity(a.getChild("city").getValue());
       candidate.setGender(a.getChild("gender").getValue());
