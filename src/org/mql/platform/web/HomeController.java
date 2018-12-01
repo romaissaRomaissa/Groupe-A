a9f62c4886b62afd83f3d27d00354c31af746a00
chermehdi <mehdi.cheracher@gmail.com>
2018-12-01 14:02:37 +0100

    - added thymleaf support

--- src/org/mql/platform/web/HomeController.java
+++ src/org/mql/platform/web/HomeController.java
@@ -1,17 +1,19 @@
 package org.mql.platform.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
- * @author mehdithe
+ * @author chermehdi
  */
 @Controller
 public class HomeController {
 
   @GetMapping("/demo")
-  public String get() {
-  	// added a comment
-    return "test";
+  public String get(Model model) {
+    // added a comment
+    model.addAttribute("message", "hello world");
+    return "demo";
   }
 }
